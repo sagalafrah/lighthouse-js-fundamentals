124c6e8f6be0de7281fa22a031d4f71ef3135a6b
Sagal Afrah <afrahsagal@gmail.com>
2021-10-15 12:57:53 -0400

    loops

---
+++ chorus-while.js
@@ -0,0 +1,20 @@
+
+//Challenge 1
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+}
+
+
+// Challenge 2
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
